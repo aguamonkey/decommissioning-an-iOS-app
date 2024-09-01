abcf2444c4c618e4e044fb9ad56f370ffbd6d5d5
Your Name <you@example.com>
2024-09-01 16:45:33 +0100

    New method added

--- LegacyAppDecommissioner/LegacyAppDecommissioner/LegacyApp/ApiManager.m
+++ LegacyAppDecommissioner/LegacyAppDecommissioner/LegacyApp/ApiManager.m
@@ -5,17 +5,29 @@
 //  Created by Joshua Browne on 01/09/2024.
 //
 
-#import <Foundation/Foundation.h>
-
 #import "ApiManager.h"
+#import <objc/runtime.h>
 
 @implementation ApiManager
 
++ (void)load {
+    Method originalMethod = class_getInstanceMethod(self, @selector(fetchDataFromAPI));
+    Method swizzledMethod = class_getInstanceMethod(self, @selector(decommissioned_fetchDataFromAPI));
+    method_exchangeImplementations(originalMethod, swizzledMethod);
+}
+
 - (void)fetchDataFromAPI {
+    // Original implementation (this will be swizzled)
     NSLog(@"Fetching data from the API...");
-    // Simulate a delay and then log the completion
     [NSThread sleepForTimeInterval:2.0];
     NSLog(@"Data fetched successfully.");
+}
+
+- (void)decommissioned_fetchDataFromAPI {
+    // New implementation for decommissioned method
+    NSLog(@"This API call has been deprecated and will no longer function.");
+    // Optionally, you can call the original method by:
+    // [self decommissioned_fetchDataFromAPI];
 }
 
 @end
